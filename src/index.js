2be1d2a74f949e8b468f35045ab9584f334d5d05
HamzaAnwar1998 <anwarhamza919@gmail.com>
2022-04-13 21:42:13 +0500

    show scores on starting app

--- src/index.js
+++ src/index.js
@@ -9,17 +9,28 @@ const username = document.getElementById('username');
 const userscore = document.getElementById('userscore');
 
 // getting data from api
-document.getElementById('refresh').addEventListener('click', async () => {
+const getScores = async () => {
   const data = await leaderboard.listScores();
   data.result.forEach((individualData) => {
     ul.innerHTML += `<li class="score list-group-item">
     <span>${individualData.user}:${individualData.score}</span>
     </li>`;
   });
+};
+
+// printing data when DOMContentLoaded
+document.addEventListener('DOMContentLoaded', async () => {
+  await getScores();
+});
+
+// printing data when refresh button is clicked
+document.getElementById('refresh').addEventListener('click', async () => {
+  await getScores();
 });
 
 // sending data
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   await leaderboard.sendData(username.value, userscore.value);
+  form.reset();
 });
